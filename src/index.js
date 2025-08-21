d5746773b731afa181f28b20a5f38e87f484f6d2
Anastasiia <egoinitium@gmail.com>
2025-08-22 00:14:57 +0300

    add index.js with general logic

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync'
+
+export default (gameRules, askAQuestion, getAnAnswer) => {
+  console.log('Welcome to the Brain Games!')
+  const name = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${name}!`)
+  console.log(gameRules)
+  let winScore = 0
+
+  while (winScore < 3) {
+    console.log(askAQuestion())
+    const playerResponse = readlineSync.question('Your answer: ').toLowerCase()
+    const correctAnswer = getAnAnswer()
+
+    if (playerResponse === correctAnswer) {
+      console.log('Correct!')
+      winScore += 1
+    }
+    else {
+      console.log(`'${playerResponse}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+      console.log(`Let's try again, ${name}!`)
+      return
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`)
+  return
+}
