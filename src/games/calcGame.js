0cd1d4a9ab261636bb07e46fd3a32ab912042ab9
Anastasiia <egoinitium@gmail.com>
2025-08-22 00:13:49 +0300

    add calsGame and parityGame with unique logic

---
+++ src/games/calcGame.js
@@ -0,0 +1,34 @@
+import app from '../index.js'
+import getRandomNumber from '../getRandomNumber.js'
+import getRandomSign from '../getRandomSign.js'
+
+const gameRules = 'What is the result of the expression?'
+let randomNum1
+let randomNum2
+let randomSign
+
+const askAQuestion = () => {
+  randomNum1 = getRandomNumber()
+  randomNum2 = getRandomNumber()
+  randomSign = getRandomSign()
+  const question = `Question: ${randomNum1} ${randomSign} ${randomNum2}`
+  return question
+}
+
+const getAnAnswer = () => {
+  let correctAnswer
+  switch (randomSign) {
+    case '+':
+      correctAnswer = randomNum1 + randomNum2
+      break
+    case '-':
+      correctAnswer = randomNum1 - randomNum2
+      break
+    case '*':
+      correctAnswer = randomNum1 * randomNum2
+      break
+  }
+  return String(correctAnswer)
+}
+
+export default () => app(gameRules, askAQuestion, getAnAnswer)
